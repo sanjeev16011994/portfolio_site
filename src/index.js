a37267aff871e559b543ae92f4af2eaef32ce4b3
sanjeev thakur <sanjeev.thakur@datamoulds.com>
2024-01-15 15:58:16 +0530

    resume download feature added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./component/firebase";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
